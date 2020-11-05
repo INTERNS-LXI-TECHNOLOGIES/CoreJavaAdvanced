fbd4454ccc65d460a2d779954792aa01236fb142
aswathy30 <aswathy.m@lxisoft.com>
2020-11-05 19:48:18 +0530

    http://jira.bytatech.com:8080/browse/IN-11885

---
+++ MovieScript/src/com/lxisoft/moviescript/Actors.java
@@ -0,0 +1,133 @@
+package com.lxisoft.moviescript;
+import com.lxisoft.moviescript.Movie;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner; 
+import java.io.*;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader; 
+import java.io.FileReader; 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files; 
+import java.nio.file.Paths; 
+import java.util.ArrayList; 
+import java.util.Collections; 
+import java.util.List;
+
+
+public class Actors   implements Serializable{
+	 ArrayList<String>  actordialogue= new ArrayList<>();
+     Scanner scanner=new Scanner(System.in);
+	 Random random=new Random();
+	 private int randomNumber;
+    public void setRandomNumber(int a){
+        this.randomNumber=a;
+
+    }
+    public int getRandomNumber(){
+        return randomNumber;
+    }
+
+	public void addingDialogue(){
+   
+  
+
+   actordialogue.add("SCENE 1");
+   actordialogue.add(" SCENE 1 Marlin: WOW");
+
+   actordialogue.add("SCENE 1 :CORAL Mmm.");
+
+actordialogue.add("SCENE 1:MARLIN Wow.");
+
+actordialogue.add("SCENE 1:CORAL Mmm-hmm.");
+
+actordialogue.add("SCENE 1:MARLIN Wow.");
+
+actordialogue.add("SCENE 1:CORAL :Yes, Marlin. No, I see it. It's beautiful.");
+
+actordialogue.add("SCENE 1:MARLIN:So, Coral, when you said you wanted an ocean view, you didn't think that we we're gonna get the whole ocean, did you? Huh? [sighs] Oh yeah. A fish can breath out here. Did yourman deliver or did he deliver?");
+
+actordialogue.add("SCENE 2");
+
+actordialogue.add(" SCENE 2 NEMO :First day of school! First day of school! Wake up, wake up! C'mon, first day of school! ");
+
+actordialogue.add("SCENE 2 MARLINI: don't wanna go to school. Five more minutes.");
+
+actordialogue.add("SCENE 2 NEMO :Not you, dad. Me!");
+
+actordialogue.add("SCENE 2 MARLIN: Okay...huh?");
+actordialogue.add("SCENE 2 NEMO: Get up, get up! It's time for school! It's time for school! It's time for school!It's time for school! Oh boy! Oh boy! ");
+
+actordialogue.add("SCENE 2MARLIN:All right, I'm up.");
+
+actordialogue.add("SCENE 2:NEMO :Oh boy--whoa! ");
+
+actordialogue.add("............THE END..............");
+
+
+this.createFile();
+//this.randomSelection();
+//this.readFile();
+}
+
+public void createFile(){//and read
+	try{
+            FileOutputStream writeData = new FileOutputStream("F:\\Program files\\EEJavaFoundational\\MovieScript\\src\\com\\lxisoft\\moviescript\\moviescriptfile.csv");
+            ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+
+            writeStream.writeObject(actordialogue);
+            writeStream.flush();
+            writeStream.close();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+}
+ 
+public void readFile() throws Exception{
+
+List<String> lines = Collections.emptyList(); 
+try {
+lines = Files.readAllLines(Paths.get(""), StandardCharsets.UTF_8); 
+} 
+catch (IOException e) {
+ // TODO Auto-generated catch block
+  e.printStackTrace();
+  } System.out.println("Content of List:"); 
+  System.out.println(lines);
+
+
+
+}
+
+public void randomSelection(){
+     int index = random.nextInt(actordialogue.size());
+     String randomDialogue=actordialogue.get(index);
+     System.out.println("RANDOM DIALOGUE:"+randomDialogue);
+     System.out.println("PRESS 0 TO CONTINUE");
+      int choice=scanner.nextInt();
+      if(choice==0){
+
+
+       for(int i=index;i<actordialogue.size();i++){
+          System.out.println(actordialogue.get(i));
+
+       }
+     }//for if
+
+
+
+
+}
+
+
+	
+
+}
---
+++ MovieScript/src/com/lxisoft/moviescript/Movie.java
@@ -0,0 +1,154 @@
+package com.lxisoft.moviescript;
+import com.lxisoft.files.File;
+import java.io.FileNotFoundException;
+
+
+import java.util.*;
+import java.util.Scanner; 
+import java.io.*;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import java.util.Scanner;
+public class Movie{
+   
+   
+   Actors actors=new Actors();
+   Scriptwriter scriptwriter=new Scriptwriter();
+   Director director=new Director();
+   
+	
+    Scanner scanner=new Scanner(System.in);
+    String dialogue;
+
+	public void randomCheck(){
+		System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                   MOVIE SCRIPT                                       *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                   FINDING NEMO                                      *****");
+        System.out.println("*****                                   ________________                                   *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("*****                                                                                      *****");
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("\n FINDING NEMO");
+        this.display();
+        actors.addingDialogue();
+
+	}
+
+    public void display(){
+        System.out.println("PRESS 0 TO CONTINUE");
+         int choice=scanner.nextInt();
+         
+        if(choice==0){
+
+
+       
+        this.adminFunctions();
+
+         System.out.println("\n A RANDOM SCENE");
+        System.out.println("************************************************************************************************");
+ System.out.println("************************************************************************************************");
+ System.out.println("************************************************************************************************");
+        
+
+      //comedian.setcomedianDialoges();
+       //ilereppo.fileCreation();
+       //his.fileWrite();
+     
+        
+
+
+            }
+        }
+        public void adminFunctions(){
+        
+        boolean quit=false;
+
+         int choice=0;
+        do
+        {
+
+            System.out.println("\n \n ***ENTER YOUR DESIRED OPERATION TO PERFORM***");
+            System.out.println("1.  ADD");
+            System.out.println("2.  DELETE");
+            System.out.println("3.  UPDATE");
+            
+            System.out.println("0.  TO EXIT");
+
+            System.out.println("Enter your choice");
+             choice=scanner.nextInt();
+
+
+            
+            
+        
+                if(choice==1)
+                {
+                this.addDynamically();
+                }
+                if(choice==2)
+                {
+                this.removeDynamically();
+                }
+                if(choice==3)
+                {
+                this.modifyNameDynamically();
+                }
+                
+                else if (choice==0)
+                {
+                System.out.println(" ");
+                }
+            }
+            while(choice>0);
+      
+                
+            
+
+            
+            }
+
+            public void addDynamically(){
+
+
+            System.out.println("Enter the nextLine" );
+             dialogue=scanner.next();
+
+
+          actors.actordialogue.add(dialogue);
+            
+           
+            }
+             public void removeDynamically(){
+            System.out.println("Enter the index Number of item to be deleted");
+            int indexnumber=scanner.nextInt();
+
+          
+          actors.actordialogue.remove(indexnumber);
+            }
+
+            
+            
+             public void  modifyNameDynamically(){
+                System.out.println("Enter the index number to be modified");
+                int indexnumber=scanner.nextInt();
+                System.out.println("Enter the new name");
+                String newDialouge=scanner.next();
+               // comedian.comedianScript[indexnumber].set(newDialouge);
+            }
+
+	
+	
+}
