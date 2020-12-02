fe96df4f42e6fa81af6233500cb64d8b8067fee2
faris-backer <faris.backer@lxisoft.com>
2020-12-02 18:31:53 +0530

    http://jira.bytatech.com:8080/browse/IN-12358

---
+++ MovieScript/src/com/lxisoft/MovieScript App/Model/Model.java
@@ -0,0 +1,11 @@
+package com.Lxisoft.Moviescript.Control;
+import com.Lxisoft.Moviescript.Model.*;
+import com.Lxisoft.Moviescript.Control.*;
+import com.Lxisoft.Files.*;
+import java.util.io.*;
+import java.util.*;
+import java.io.FileNotFoundException; 
+public class Model
+{
+   
+}
