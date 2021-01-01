f41207eb3062aa5f5d4ab9edc9a0169d491c351d
faris-backer <faris.backer@lxisoft.com>
2021-01-01 17:49:00 +0530

    http://jira.bytatech.com:8080/browse/IN-12806

---
+++ ContactApp/src/com/Lxisoft/Test/TDD.java
@@ -0,0 +1,12 @@
+package com.Lxisoft.Test;
+import com.Lxisoft.Control.ContactAppController;
+
+
+public class TDD
+{
+	public static void main(String [] args)
+	{
+		ContactAppController controller = new 	 ContactAppController();
+		controller.mainMenu();
+	}
+}
